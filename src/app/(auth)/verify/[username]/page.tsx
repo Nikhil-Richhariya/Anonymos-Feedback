bcc024b2e1ea9158f9882da415f21ba8e3064d50
Nikhil-Richhariya <richhariyan@rknec.edu>
2024-06-19 10:04:37 +0530

    verification code page

--- src/app/(auth)/verify/[username]/page.tsx
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -1,10 +1,16 @@
+'use client'
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
 import { verifySchema } from "@/schemas/verifySchema";
+import { ApiResponse } from "@/types/ApiResponse";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Description } from "@radix-ui/react-toast";
+import axios, { AxiosError } from "axios";
 import { useParams, useRouter } from "next/navigation";
 import React from "react";
-import { useForm } from "react-hook-form";
+import {useForm } from "react-hook-form";
 import * as z from "zod";
 
 const VerifyAccount = () => {
@@ -17,25 +23,70 @@ const VerifyAccount = () => {
   const form = useForm<z.infer<typeof verifySchema>>({
     resolver: zodResolver(verifySchema),
   });
+ 
 
-
-  const onSubmit = async (data : z.infer<typeof verifySchema) => {
+  const onSubmit = async (data : z.infer<typeof verifySchema>) => {
     try {
-        const reponse = await axios.post(/`api/verify-code`, {
-            username : Params.username,
+        const response = await axios.post(`/api/verify-code`, {
+            username : params.username,
             code : data.code
         }); 
 
         toast({
             title : "Success", 
-            Description : response.data.message
+            description : response.data.message
         })
+
+        router.replace('sign-in') // redirecting user after
+
     } catch (error) {
-        
+      console.error("Error in sign-up of user", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "Signup failed",
+        description: errorMessage,
+        variant: "destructive",
+      });
     }
   }
 
-  return <div></div>;
+  return (
+
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-5xl font-extrabold tracking-tigh lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+        name="code"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Verification Code</FormLabel>
+              <FormControl>
+                <Input placeholder="code" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+
+      </div>
+    
+    </div>
+  )
 };
 
 export default VerifyAccount;
